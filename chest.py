4e5b74aa0e0b4f62d9b6c5f638417deb1f9cb368
Your Name <you@example.com>
2019-11-27 13:07:29 +0100

    Add random chest inventory

--- chest.py
+++ chest.py
@@ -1,1 +1,8 @@
-chest_inventory = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+x = ['gold coin', 'dagger', 'torch', 'arrow', 'bow', 'ruby']
+ksa = [1,2,3,4,5]
+pos = 2
+import random
+z = random.choice(ksa)
+chest_inventory = random.choices(x, k=z)
+for i in range(len(chest_inventory)):
+   x.insert(i + pos, chest_inventory[i])
